9fffc8e45bda8001edb3084e30e9ad9640ca459a
An Phan <an.phan@cyberlogitec.com>
2023-08-01 20:21:52 +0700

    Setup layout

---
+++ app/create-project/page.tsx
@@ -0,0 +1,19 @@
+import { getCurrentUser } from '@/lib/session';
+import Modal from '../components/Modal';
+import ProjectForm from '../components/ProjectForm';
+import { redirect } from 'next/navigation';
+
+const CreateProject = async () => {
+  const session = await getCurrentUser();
+
+  if (!session?.user) redirect('/');
+
+  return (
+    <Modal>
+      <h3 className="modal-head-text">Create a new project</h3>
+      <ProjectForm type="create" session={session} />
+    </Modal>
+  );
+};
+
+export default CreateProject;
