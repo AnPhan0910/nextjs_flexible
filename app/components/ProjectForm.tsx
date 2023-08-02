476d3ed2e922468a0c70f4dbb8bf5cddc0f1addb
An Phan <an.phan@cyberlogitec.com>
2023-08-02 22:26:06 +0700

    Finish update image feature

--- app/components/ProjectForm.tsx
+++ app/components/ProjectForm.tsx
@@ -6,6 +6,8 @@ import FormField from './FormField';
 import { categoryFilters } from '@/constants';
 import CustomMenu from './CustomMenu';
 import Button from './Button';
+import { createNewProject, fetchToken } from '@/lib/actions';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   type: string;
@@ -13,7 +15,35 @@ type Props = {
 };
 
 const ProjectForm = ({ type, session }: Props) => {
-  const handleFormSubmit = (e: React.FormEvent) => {};
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    image: '',
+    title: '',
+    description: '',
+    liveSiteUrl: '',
+    githubUrl: '',
+    category: '',
+  });
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setIsSubmitting(true);
+    try {
+      const { token } = await fetchToken();
+      if (type === 'create') {
+        await createNewProject(form, session?.user?.id, token);
+
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const file = e.target.files?.[0];
@@ -37,16 +67,6 @@ const ProjectForm = ({ type, session }: Props) => {
   const handleStateChange = (fieldName: string, value: string) => {
     setForm((prev) => ({ ...prev, [fieldName]: value }));
   };
-
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [form, setForm] = useState({
-    image: null,
-    title: '',
-    description: '',
-    liveSiteUrl: '',
-    githubUrl: '',
-    category: '',
-  });
 
   return (
     <form onSubmit={handleFormSubmit} className="flexStart form">
@@ -97,12 +117,6 @@ const ProjectForm = ({ type, session }: Props) => {
         state={form.githubUrl}
         placeholder="https://github.com/"
         setState={(value) => handleStateChange('githubUrl', value)}
-      />
-      <FormField
-        title="Title"
-        state={form.title}
-        placeholder="Flexibble"
-        setState={(value) => handleStateChange('title', value)}
       />
 
       {/* CustomInput Category... */}
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,4 +1,10 @@
-import { createUserMutation, getUserQuery } from '@/graphql';
+import { ProjectForm } from '@/common.types';
+import {
+  createProjectMutation,
+  createUserMutation,
+  getUserQuery,
+  projectsQuery,
+} from '@/graphql';
 import { GraphQLClient } from 'graphql-request';
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -15,9 +21,18 @@ const serverUrl = isProduction
 
 const client = new GraphQLClient(apiUrl);
 
-const makeGraphQLRequest = async (query: String, variables = {}) => {
+const makeGraphQLRequest = async (query: string, variables = {}) => {
   try {
     return await client.request(query, variables);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchToken = async () => {
+  try {
+    const res = await fetch(`${serverUrl}/api/auth/token`);
+    return res.json();
   } catch (error) {
     throw error;
   }
@@ -39,4 +54,49 @@ export const createUser = (name: string, email: string, avatarUrl: string) => {
   };
 
   return makeGraphQLRequest(createUserMutation, variables);
+};
+
+export const updateImage = async (imagePath: string) => {
+  try {
+    const res = await fetch(`${serverUrl}/api/upload`, {
+      method: 'POST',
+      body: JSON.stringify({ path: imagePath }),
+    });
+
+    return res.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createNewProject = async (
+  form: ProjectForm,
+  creatorId: string,
+  token: string,
+) => {
+  const imageUrl = await updateImage(form.image);
+
+  if (imageUrl.url) {
+    client.setHeader('Authorization', `Bearer ${token}`);
+
+    const variables = {
+      input: {
+        ...form,
+        image: imageUrl.url,
+        createdBy: {
+          link: creatorId,
+        },
+      },
+    };
+    return makeGraphQLRequest(createProjectMutation, variables);
+  }
+};
+
+export const fetchAllProjects = async (
+  category?: string,
+  endcursor?: string,
+) => {
+  client.setHeader('x-api-key', apiKey);
+
+  return makeGraphQLRequest(projectsQuery, { category, endcursor });
 };
